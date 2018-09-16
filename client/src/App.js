008137eab76a92d02eaaef3fe8a141074aca5a7e
Bartek Wilczek <bwilczek@gmail.com>
2018-09-16 21:13:29 +0200

    Display data fetched over GraphQL

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
 import gql from "graphql-tag";
 
 const client = new ApolloClient({
@@ -12,28 +13,37 @@ const client = new ApolloClient({
   }
 });
 
+const Author = () => (
+  <Query query={gql`query { author(id: 1) { name books {title} } }`} >
+    {({ loading, error, data }) => {
+      if (loading) return (<div>Loading...</div>);
+      if (error) return (<div>Error :(</div>);
+      return (
+        <div>
+          <div>{data.author.name}</div>
+          <ul>
+            { data.author.books.map( ({title}) => (<li key={title}>{title}</li>) ) }
+          </ul>
+        </div>
+      )
+    }}
+  </Query>
+);
+
 class App extends Component {
   render() {
-
-    console.log('render');
-
-    client.query({
-      query: gql`query { author(id: 2) { name } }`
-    }).then(
-      result => console.log(result),
-      error => console.log(error)
-    );
-
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <div className="App-intro">
+            <Author />
+          </div>
+        </div>
+      </ApolloProvider>
     );
   }
 }
