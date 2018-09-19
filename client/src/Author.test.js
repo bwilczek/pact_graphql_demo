1a76842d05f7228aff5a2cfbdf53345c00c7c6a7
Bartek Wilczek <bwilczek@gmail.com>
2018-09-19 11:00:46 +0200

    Introduce MockedProvider

--- client/src/Author.test.js
+++ client/src/Author.test.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer'
 import { Author, GET_AUTHOR_QUERY }from './Author'
-import fetch from 'node-fetch'
-import ApolloClient from "apollo-boost"
-import { ApolloProvider } from "react-apollo"
+import { MockedProvider } from 'react-apollo/test-utils'
+const wait = require('waait');
 
-const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
-  fetch: fetch,
-  fetchOptions: {
-    mode: 'no-cors'
-  }
-});
+let mocks = [
+  {
+    request: {
+      query: GET_AUTHOR_QUERY,
+      variables: {
+        id: 2,
+      },
+    },
+    result: {
+      data: {
+        author: {
+          id: 2,
+          name: 'Halina Kowal',
+          books: [
+            { title: 'C++ for dummies' },
+            { title: 'C# for bosses' },
+          ]
+        }
+      },
+    },
+  },
+]
 
-it('renders without crashing', () => {
-  TestRenderer.create(
-    <ApolloProvider client={client}>
+it('renders without crashing', async () => {
+  const component = TestRenderer.create(
+    <MockedProvider mocks={mocks} addTypename={false}>
       <Author id={2} />
-    </ApolloProvider>
+    </MockedProvider>
   )
+
+  await wait(0)
+
+  const titles = component.root.findAllByType('li').map(el => el.children[0])
+  expect(titles).toContain('C# for bosses')
 });
+
