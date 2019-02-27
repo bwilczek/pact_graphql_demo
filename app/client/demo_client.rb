49565685840ae2343e9f9fd04e63389e31302e7e
Bartek Wilczek <bartek.wilczek@toptal.com>
2019-02-27 15:19:46 +0100

    WiP initial progress on Pact for ruby client

--- app/client/demo_client.rb
+++ app/client/demo_client.rb
@@ -1,31 +1,31 @@
 require 'graphql/client'
 require 'graphql/client/http'
 
-module Demo
-  # HTTP = GraphQL::Client::HTTP.new('http://localhost:3000/graphql')
-  # Schema = GraphQL::Client.load_schema(HTTP)
-  # Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+# @see https://github.com/github/graphql-client#configuration
 
-  class << self
-    def client(address = 'http://localhost:3000/graphql')
-      @http ||= GraphQL::Client::HTTP.new(address)
-      @client ||= GraphQL::Client.new(schema: GraphQL::Client.load_schema(@http), execute: @http)
-    end
+class DemoClient
+  def initialize(address = 'http://localhost:3000/graphql')
+    @address = address
+  end
 
-    def qotd
-      unless defined?(QotdQuery)
-        t = client.parse <<-'GRAPHQL'
-          query {
-            qotd {
-              author
-              quote
-              __typename
-            }
+  def client
+    @http ||= GraphQL::Client::HTTP.new(@address)
+    @client ||= GraphQL::Client.new(schema: GraphQL::Client.load_schema(@http), execute: @http)
+  end
+
+  def qotd
+    unless defined?(QotdQuery)
+      t = client.parse <<-'GRAPHQL'
+        query {
+          qotd {
+            author
+            quote
+            __typename
           }
-        GRAPHQL
-        Demo.const_set('QotdQuery', t)
-      end
-      client.query(QotdQuery)
+        }
+      GRAPHQL
+      DemoClient.const_set('QotdQuery', t)
     end
+    client.query(QotdQuery)
   end
 end
--- spec/demo_client_spec.rb
+++ spec/demo_client_spec.rb
@@ -1,7 +1,40 @@
 require_relative '../app/client/demo_client'
 
-RSpec.describe Demo do
+require 'pact/consumer/rspec'
+
+Pact.service_consumer "DemoClient" do
+  has_pact_with "AuthorProvider" do
+    mock_service :author_provider do
+      port 1234
+      pact_specification_version "2.0.0"
+    end
+  end
+end
+
+RSpec.describe DemoClient, :pact => true do
+  # DemoClient.new.qotd.data.qotd.author
+  before do
+    author_provider
+      .given('No body is given')
+      .upon_receiving('an empty request')
+      .with(method: :post, path: '/graphql')
+      .will_respond_with(
+        status: 200,
+        headers: { 'Content-Type' => 'application/json' },
+        body: {
+          data: {
+            qotd: {
+              author: Pact.like('Frank Kimono'),
+              quote: Pact.like('King Bruce Lee')
+            }
+          }
+        }
+      )
+  end
+
+  let(:client) { DemoClient.new('http://localhost:1234/graphql') }
+
   it 'passes' do
-    expect(true).to be(true)
+    expect(client.qotd.data.qotd.author).to be('Franek Kimono')
   end
 end
